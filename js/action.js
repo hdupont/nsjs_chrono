37d98ec00d93c9d11e7565cfb666b31aab92465c
hdupont <dupont.hubert@gmail.com>
2016-12-13 08:13:59 +0100

    doc

--- js/action.js
+++ js/action.js
@@ -1,10 +1,8 @@
 /**
- * --------
+ * -------------
  * @class Action
- * --------
+ * -------------
  * Une Action est une action pouvant être effectuée sur le chrono.
- * 
- * NOTE Pas de code spécifique au controlleur dans cette classe.
  */
 chronoapp.Action = (function() {
 	
@@ -14,34 +12,56 @@ chronoapp.Action = (function() {
 	/**
 	 * Construit une action pouvant être effectuée sur le chrono.
 	 * @property {string} _name Le nom de l'action.
-	 * @property {string} _handler Le code de l'action.
+	 * @property {string} _handler Le traitement correspondant à l'action.
+	 * @property {string} _onAction Le traitement inscrit pour être effectué à
+	 * la suite de l'action.
+	 * @property {object} _onActionContext L'objet qui sert de contexte à
+	 * l'exécution de onAction();.
+	 * NOTE _onActionContext est valorisé dans Actions lors l'ajout à la liste
+	 * des actions. 
 	 */
 	function Action(name, handler, onAction, keys) {	
 		this._name = name;
 		this._handler = handler;
 		this._onAction = onAction;
 		this._keys = keys;
-		this._context = null;
+		this._onActionContext = null;
 	}
 
-	Action.prototype.setContext = function(context) {
-		this._context = context;
+	/**
+	 * Valorise _onActionContext
+	 * @param {object} onActionContext L'objet qui servira de contexte à
+	 * l'exécution de onAction();
+	 */
+	Action.prototype.setOnActionContext = function(onActionContext) {
+		this._onActionContext = onActionContext;
 	};
 	
+	/**
+	 * Retourne le nom de l'action.
+	 * @returns {string} Le nom de l'action.
+	 */
 	Action.prototype.getName = function() {
 		return this._name;
 	};
 	
+	/**
+	 * Retourne les touches claviers pouvant déclencer l'action.
+	 * @returns {array} La liste des touches pouvant déclencher l'action.
+	 */
 	Action.prototype.getKeys = function() {
 		return this._keys;
 	};
 	
+	/**
+	 * Exécute l'action puis exécute _onAction(). 
+	 */
 	Action.prototype.execute = function() {
-		if (this._context === null || typeof this._context === "undefined") {
+		if (this._onActionContext === null || typeof this._onActionContext === "undefined") {
 			throw new Error("Action..execute - no context");
 		}
 		this._handler();
-		this._onAction(this._context);
+		this._onAction(this._onActionContext);
 	};
 	
 	return Action;
--- js/actions.js
+++ js/actions.js
@@ -2,7 +2,7 @@
  * --------------
  * @class Actions
  * --------------
- * Une Actions est une liste d'actions pouvant être effectuées sur le chrono.
+ * Une Actions est une liste d'actions pouvant être effectuées sur un chrono.
  */
 chronoapp.Actions = (function() {
 	
@@ -11,29 +11,34 @@ chronoapp.Actions = (function() {
 
 	/**
 	 * Construit une liste d'actions pouvant être effectuées sur le chrono.
-	 * @property {string} _chrono Le chrono sur lequel seront effectuées les
+	 * @property {object} _chrono Le chrono sur lequel seront effectuées les
 	 * actions.
+	 * @property {object} _actionsContext le contexte dans lequel sera exécuté
+	 * le onAction des actions de la liste.
+	 * NOTE Tous les _onActions des actions s'exécute dans le même contexte: 
+	 * onActionContext.
+	 * @property {array} _actions La liste des actions pouvant être effectuées
+	 * sur _chrono.
 	 */
-	function Actions(chrono, actionsContext) {	
+	function Actions(chrono, onActionContext) {	
 		this._chrono = chrono;
-		this._actionsContext = actionsContext;
+		this._onActionContext = onActionContext;
 		this._actions = [];
 	}
 
+	/**
+	 * Ajoute une action à la liste.
+	 * @param {object} action L'action à ajouter à la liste.
+	 */
 	Actions.prototype.addAction = function(action) {
-		action.setContext(this._actionsContext);
+		action.setOnActionContext(this._onActionContext);
 		return this._actions.push(action);
 	};
 	
-	Actions.prototype.execute = function() {
-		var self = this;
-		this._actions.forEach(function(action) {
-			action.execute(thselfis._chrono, self._actionContext);
-		});
-	};
-	
 	/**
-	 * Applique la fonction sur toutes les actions.
+	 * Effectue un traitement sur toutes les actions.
+	 * @param {function} fun Le traitement à appliquer sur toutes les actions
+	 * de la liste.
 	 */
 	Actions.prototype.each = function(fun) {
 		this._actions.forEach(function(action) {
